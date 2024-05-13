7f11c43737c28ced9f7d46e6da5ae5c2dbe64519
50297039 <72810599+50297039@users.noreply.github.com>
2024-05-14 01:00:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -29,7 +29,7 @@ let addressesnotlsapi = [
 
 let DLS = 8;//速度下限
 let addressescsv = [
-	//'https://cf-workers-text2kv-ag7.pages.dev/result.csv?token=jinmy', //iptest测速结果文件。
+	//'', //iptest测速结果文件。
 ];
 
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
